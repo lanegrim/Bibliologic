3e185491578daeb3109010630bad23be6e2368dc
lanegrimaud <lanewgrimaud@gmail.com>
2021-04-28 16:02:17 -0400

    index now displays entries and new entries can be created via new.ejs

--- models/entries.js
+++ models/entries.js
@@ -4,7 +4,10 @@ const entrySchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     image: String,
+    genre: String,
+    rating: { type: Number, min: 1, max: 5 },
     notes: String,
+    date: { type: Date, default: Date.now },
 });
 
 const Entry = mongoose.model('Entry', entrySchema);
