f7c0c1b80ba0b66c8296171a7ddb2bb9f3497610
lanegrimaud <lanewgrimaud@gmail.com>
2021-05-02 21:44:37 -0400

    got script for open books api requests functioning

--- public/app.js
+++ public/app.js
@@ -2,3 +2,5 @@ document.addEventListener('DOMContentLoaded', function () {
     let elems = document.querySelectorAll('.sidenav');
     let instances = M.Sidenav.init(elems, { edge: 'right' });
 });
+
+
